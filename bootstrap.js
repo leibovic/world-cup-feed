144025f5b54045b60a55986ff2c698c47c2617cc
Margaret Leibovic <margaret.leibovic@gmail.com>
2014-03-26 17:22:31 -0700

    support switching feed based on country

--- bootstrap.js
+++ bootstrap.js
@@ -11,38 +11,15 @@ const ADDON_ID = "world.cup.feed@mozilla.org";
 const PANEL_ID = "world.cup.feed.panel@mozilla.org";
 const DATASET_ID = "world.cup.feed.dataset@mozilla.org";
 
-var gFeedUrls = {
-  // Argentina
-  AR: "http://www.goal.com/es-ar/feeds/news?fmt=rss&ICID=HP",
-  // Mexico
-  MX: "http://www.goal.com/es-mx/feeds/news?fmt=rss&ICID=HP",
-  // Colombia
-  CO: "http://www.goal.com/es-co/feeds/news?fmt=rss&ICID=HP",
-  // Chile
-  CL: "http://www.goal.com/es-cl/feeds/news?fmt=rss&ICID=HP",
-  // Brazil
-  BR: "http://www.goal.com/br/feeds/news?fmt=rss&ICID=HP",
-  // Germany
-  DE: "http://www.goal.com/de/feeds/news?fmt=rss&ICID=HP",
-  // Spain
-  ES: "http://www.goal.com/es/feeds/news?fmt=rss&ICID=HP",
-  // United Kington
-  GB: "http://www.goal.com/en-gb/feeds/news?fmt=rss&ICID=HP",
-  // Italy
-  IT: "http://www.goal.com/it/feeds/news?fmt=rss&ICID=HP",
-  // France
-  FR: "http://www.goal.com/fr/feeds/news?fmt=rss&ICID=HP",
-  // United States
-  US: "http://www.goal.com/en-us/feeds/news?fmt=rss&ICID=HP",
-  // Indonesia
-  ID: "http://www.goal.com/id/feeds/news?fmt=rss&ICID=HP",
-  // India
-  IN: "http://www.goal.com/en-india/feeds/news?fmt=rss&ICID=HP"
-};
+const SNIPPETS_COUNTRY_CODE_PREF = "browser.snippets.countryCode";
+const WCF_COUNTRY_CODE_PREF = "worldCupFeed.countryCode";
 
-// An example of how to create a string bundle for localization.
 XPCOMUtils.defineLazyGetter(this, "Strings", function() {
   return Services.strings.createBundle("chrome://worldcupfeed/locale/worldcupfeed.properties");
+});
+
+XPCOMUtils.defineLazyGetter(this, "RegionNames", function() {
+  return Services.strings.createBundle("chrome://global/locale/regionNames.properties");
 });
 
 XPCOMUtils.defineLazyGetter(this, "FeedHelper", function() {
@@ -51,15 +28,81 @@ XPCOMUtils.defineLazyGetter(this, "FeedHelper", function() {
   return sandbox["FeedHelper"];
 });
 
-// Take advantage of the fact that Snippets.js already caches the user's country code.
-XPCOMUtils.defineLazyGetter(this, "gCountryCode", function() {
-  try {
-    return Services.prefs.getCharPref("browser.snippets.countryCode");
-  } catch (e) {
-    // Return an empty string if the country code pref isn't set yet.
-    return "";
+var Countries = {
+  AR: {
+    label: RegionNames.GetStringFromName("ar"),
+    feed: "http://www.goal.com/es-ar/feeds/news?fmt=rss&ICID=HP"
+  },
+  MX: {
+    label: RegionNames.GetStringFromName("mx"),
+    feed: "http://www.goal.com/es-mx/feeds/news?fmt=rss&ICID=HP"
+  },
+  CO: {
+    label: RegionNames.GetStringFromName("co"),
+    feed: "http://www.goal.com/es-co/feeds/news?fmt=rss&ICID=HP"
+  },
+  CL: {
+    label: RegionNames.GetStringFromName("cl"),
+    feed: "http://www.goal.com/es-cl/feeds/news?fmt=rss&ICID=HP"
+  },
+  BR: {
+    label: RegionNames.GetStringFromName("br"),
+    feed: "http://www.goal.com/br/feeds/news?fmt=rss&ICID=HP"
+  },
+  DE: {
+    label: RegionNames.GetStringFromName("de"),
+    feed: "http://www.goal.com/de/feeds/news?fmt=rss&ICID=HP"
+  },
+  ES: {
+    label: RegionNames.GetStringFromName("es"),
+    feed: "http://www.goal.com/es/feeds/news?fmt=rss&ICID=HP"
+  },
+  GB: {
+    label: RegionNames.GetStringFromName("gb"),
+    feed: "http://www.goal.com/en-gb/feeds/news?fmt=rss&ICID=HP"
+  },
+  IT: {
+    label: RegionNames.GetStringFromName("it"),
+    feed: "http://www.goal.com/it/feeds/news?fmt=rss&ICID=HP"
+  },
+  FR: {
+    label: RegionNames.GetStringFromName("fr"),
+    feed: "http://www.goal.com/fr/feeds/news?fmt=rss&ICID=HP"
+  },
+  US: {
+    label: RegionNames.GetStringFromName("us"),
+    feed: "http://www.goal.com/en-us/feeds/news?fmt=rss&ICID=HP"
+  },
+  ID: {
+    label: RegionNames.GetStringFromName("id"),
+    feed: "http://www.goal.com/id/feeds/news?fmt=rss&ICID=HP"
+  },
+  IN: {
+    label: RegionNames.GetStringFromName("in"),
+    feed: "http://www.goal.com/en-india/feeds/news?fmt=rss&ICID=HP"
   }
-});
+};
+
+function getCountryCode() {
+  try {
+    // First check to see if the user has set a pref for this add-on.
+    let code = Services.prefs.getCharPref(WCF_COUNTRY_CODE_PREF);
+    if (code in Countries) {
+      return code;
+    }
+  } catch (e) {}
+
+  try {
+    // Next, check to see if there's a country code set by snippets.
+    let code = Services.prefs.getCharPref(SNIPPETS_COUNTRY_CODE_PREF);
+    if (code in Countries) {
+      return code;
+    }
+  } catch (e) {}
+
+  // XXX: Choose a fallback country based on the locale.
+  return "US";
+}
 
 function optionsCallback() {
   return {
@@ -72,9 +115,8 @@ function optionsCallback() {
 }
 
 function refreshDataset() {
-  // XXX: Actually choose a feed based on the user's country, falling back to locale.
-  // Allow the user to override this with a setting.
-  let feedUrl = gFeedUrls.US;
+  let code = getCountryCode();
+  let feedUrl = Countries[code].feed;
 
   FeedHelper.parseFeed(feedUrl, function(parsedFeed) {
     let items = FeedHelper.feedToItems(parsedFeed);
@@ -99,13 +141,25 @@ function observe(doc, topic, id) {
     return;
   }
 
-  let country = doc.getElementById("country");
+  let setting = doc.getElementById("country-setting");
+  setting.setAttribute("title", Strings.GetStringFromName("country"));
 
-  country.addEventListener("command", function(){
-    Services.console.logStringMessage("******** command: " + country.value);
+  let menupopup = doc.getElementById("country-menupopup");
+  for (let code in Countries) {
+    let menuitem = doc.createElementNS("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul", "menuitem"); 
+    menuitem.setAttribute("value", code);
+    menuitem.setAttribute("label", Countries[code].label);
+    menupopup.appendChild(menuitem);
+  }
+
+  let menulist = doc.getElementById("country-menulist");
+  menulist.setAttribute("value", getCountryCode());
+
+  menulist.addEventListener("command", function() {
+    let newCountryCode = menulist.value;
+    Services.prefs.setCharPref(WCF_COUNTRY_CODE_PREF, newCountryCode);
+    HomeProvider.requestSync(DATASET_ID, refreshDataset);
   }, false);
-
-  country.value = "FR";
 }
 
 /**
